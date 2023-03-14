138db73b233279e4760b0cc2215e1975a39b796f
Caden-lee <cadenhylee@gmail.com>
2023-03-13 18:26:26 -0700

    Email address draft

---
+++ scrippy.js
@@ -0,0 +1,14 @@
+function copyEmailAddress() {
+
+    var emailAddress = "Cadenhylee@gmail.com";
+
+    navigator.clipboard.writeText(emailAddress);
+  
+    // Alert the copied text
+    alert("Email Address Copied to Clipboard: " + emailAddress);
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
