4779a0cabe7e36ea5c6004be460afc2a07967179
Carter-Tang <carterlogantang@gmail.com>
2023-03-13 21:01:13 -0700

    Functioning copy email button

--- scrippy.js
+++ scrippy.js
@@ -5,10 +5,14 @@ function copyEmailAddress() {
 
     var emailAddress = "Cadenhylee@gmail.com";
 
-    navigator.clipboard.writeText(emailAddress);
-  
-    // Alert the copied text
-    alert("Email Address Copied to Clipboard: " + emailAddress);
+    navigator.clipboard
+      .writeText(emailAddress)
+      .then(() => {
+        alert("successfully copied: " + emailAddress);
+      })
+      .catch(() => {
+        alert("something went wrong, copy of email address failed: " + emailAddress);
+      });
 }
 
 function outFunc() {
