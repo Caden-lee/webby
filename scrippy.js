77ef3b54b03f82fd08fd7fb48ed103d0b820bca3
Caden-lee <cadenhylee@gmail.com>
2023-03-14 19:58:26 -0700

    made changes to contact icons (navbar)

--- scrippy.js
+++ scrippy.js
@@ -19,23 +19,3 @@ function outFunc() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copy to clipboard";
 }
-
-$('.awesome-tooltip').tooltip({
-    placement: 'left'
-});  
-$('body').scrollspy({ 
-    target: '#mainnav', 
-    offset: 000
-});  
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {                   
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 500);
-            return false;
-        }
-    }
-});    
